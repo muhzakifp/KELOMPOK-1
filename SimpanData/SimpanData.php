ef0c47821ea264607ccaa1b6d7788cee0e4b6c41
jaki <mzakifadhila@gmail.com>
2026-06-10 20:11:45 +0700

    update simpanData

--- SimpanData/SimpanData.php
+++ SimpanData/SimpanData.php
@@ -1,5 +1,5 @@
 <?php
-require '../KonekDB/koneksi.php';
+require '../Service/koneksi.php';
 require "../fileOOP/Kendaraan.php";
 require "../fileOOP/MobilKonvensional.php";
 require "../fileOOP/MobilListrik.php";
@@ -7,13 +7,45 @@ require "../fileOOP/MotorBesar.php";
 
 class SimpanData extends Koneksi{
     
-    public function databaseKendaraan(){
+    public function tampilDatabaseKendaraan(){
         //untuk query database disini - nanti saya tulis
     }
 
     public function simpanKendaraan(Kendaraan $kendaraan){
-        //query database dan sytanks php
+
+        if($kendaraan instanceof MobilKonvesional){
+            $tipe ="Mobil Konvesional";
+        }
+        else if ($kendaraan instanceof MobilListrik){
+            $tipe = "Mobil Listrik";
+        }
+        else if ($kendaraan instanceof MotorBesar){
+            $tipe = "Motor Besar";
+        }
+        else{echo "Kendaraan tidak valid";}
+
+       $query = "INSERT INTO kendaraan (id_kendaraan,brand,model,tahun,harga_dasar,kategori_kendaraan) VALUES
+       ('$kendaraan->aksesIDkendaraan()','$kendaraan->aksesBrand()','$kendaraan->aksesModel()','$kendaraan->aksesTahun()','$kendaraan->aksesHarga()','$kategoti')" ;
+       return mysqli_query($this->koneksi,$query);
+
+       if($kendaraan instanceof MobilKonvesional){
+        $query = "INSERT INTO mobil_konvesional (id_kendaraan, kapasitas_mesin, jenis_bbm) VALUES ()";
+        return mysqli_query($this->koneksi,$query);
+           
+       }
+       else if($kendaraan instanceof MobilListrik){
+           $query = "INSERT INTO mobil_listrik (id_kendaraan, kapasitas_baterai, jarak_tempuh) VALUES ()";
+            return mysqli_query($this->koneksi,$query);
+       }
+       else if($kendaraan instanceof MotorBesar){
+        $query = "INSERT INTO motor_besar (id_kendaraan, tipe_rantai, mode_berkendara) VALUES ()";
+        return mysqli_query($this->koneksi,$query);
+       }
+       else{echo "Kendaraan tidak valid";}
     }
+
+
+
 
 }
 ?>
