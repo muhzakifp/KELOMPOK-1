0af13fa6c13fcca9c52d59c1975b9bf1f306fc39
wanda tiara levina <wandalevina4@gmail.com>
2026-06-11 05:30:48 +0700

    Menambah mobil konvesional

--- fileOOP/MobilKonvesional.php
+++ fileOOP/MobilKonvesional.php
@@ -1,8 +1,51 @@
 <?php
 require 'Kendaraan.php';
 
-class MobilKonvesional extends Kendaraan{
-    //isi kode disini
-}
+class MobilKonvesional extends Kendaraan
+{
+    private $kapasitasMesin;
+    private $jenisBahanBakar;
 
+    public function __construct(
+        $id,
+        $brand,
+        $model,
+        $tahun,
+        $hargaDasar,
+        $kapasitasMesin,
+        $jenisBahanBakar
+    ) {
+        parent::__construct(
+            $id,
+            $brand,
+            $model,
+            $tahun,
+            $hargaDasar
+        );
+
+        $this->kapasitasMesin = $kapasitasMesin;
+        $this->jenisBahanBakar = $jenisBahanBakar;
+    }
+
+    // Override method abstract dari class Kendaraan
+    public function hitungPajakTahunan()
+    {
+        return (0.02 * $this->hargaDasar) +
+               ($this->kapasitasMesin * 500);
+    }
+
+    // Override method abstract dari class Kendaraan
+    public function tampilkanSpesifikasi()
+    {
+        return "
+        ID Kendaraan : {$this->id_kendaraan}<br>
+        Brand        : {$this->brand}<br>
+        Model        : {$this->model}<br>
+        Tahun        : {$this->tahun}<br>
+        Harga Dasar  : Rp" . number_format($this->hargaDasar, 0, ',', '.') . "<br>
+        Kapasitas Mesin : {$this->kapasitasMesin} cc<br>
+        Jenis Bahan Bakar : {$this->jenisBahanBakar}<br>
+        Pajak Tahunan : Rp" . number_format($this->hitungPajakTahunan(), 0, ',', '.');
+    }
+}
 ?>
