8457e5b3df0c92b4c88fb4fe10729298fa5703f2
IlhamAli <ilhamalir4@gmail.com>
2026-06-10 20:43:06 +0700

    nambah mobil listrik

--- fileOOP/MobilListrik.php
+++ fileOOP/MobilListrik.php
@@ -2,8 +2,50 @@
 
 require 'Kendaraan.php';
 
-class MobilListrik extends Kendaraan{
-    //isi kode disini
+class MobilListrik extends Kendaraan
+{
+    private $kapasitasBaterai;
+    private $jarakTempuh;
+
+    public function __construct(
+        $id_kendaraan,
+        $brand,
+        $model,
+        $tahun,
+        $hargaDasar,
+        $kapasitasBaterai,
+        $jarakTempuh
+    ){
+        parent::__construct(
+            $id_kendaraan,
+            $brand,
+            $model,
+            $tahun,
+            $hargaDasar
+        );
+
+        $this->kapasitasBaterai = $kapasitasBaterai;
+        $this->jarakTempuh = $jarakTempuh;
+    }
+
+    public function hitungPajakTahunan()
+    {
+        return $this->hargaDasar * 0.005;
+    }
+
+    public function tampilkanSpesifikasi()
+    {
+        return [
+            'ID Kendaraan'      => $this->id_kendaraan,
+            'Brand'             => $this->brand,
+            'Model'             => $this->model,
+            'Tahun'             => $this->tahun,
+            'Harga Dasar'       => $this->hargaDasar,
+            'Kapasitas Baterai' => $this->kapasitasBaterai . ' kWh',
+            'Jarak Tempuh'      => $this->jarakTempuh . ' km',
+            'Pajak Tahunan'     => $this->hitungPajakTahunan()
+        ];
+    }
 }
 
 ?>
